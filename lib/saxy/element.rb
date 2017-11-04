cc6e107ce01283e38b5de0c665503766b7ac8076
Michał Szajbe <michal.szajbe@gmail.com>
2017-11-04 17:11:53 +0100

    Added some whitespace for better readability

--- lib/saxy/element.rb
+++ lib/saxy/element.rb
@@ -38,8 +38,8 @@ module Saxy
 
     def underscore(word)
       word = word.dup
-      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
-      word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+      word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2')
+      word.gsub!(/([a-z\d])([A-Z])/, '\1_\2')
       word.tr!("-", "_")
       word.downcase!
       word
